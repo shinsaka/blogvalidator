d9be75f92d1b0c77a3d9e68d9513c775f4979df9
shinsaka <shinx1265@gmail.com>
2016-12-08 00:19:38 +0900

    stop at maxpage

--- blogvalidator/spiders/image.py
+++ blogvalidator/spiders/image.py
@@ -8,6 +8,8 @@ class ImageSpider(scrapy.Spider):
     start_urls = [
         'http://blog.serverworks.co.jp/tech/category/techical/',
     ]
+    MAX_CRAWL_PAGE_COUNT = 5
+    crawld_page_count = 0
 
     def parse(self, response):
         for article in response.css('article'):
@@ -15,7 +17,8 @@ class ImageSpider(scrapy.Spider):
             yield scrapy.Request(article_url, callback=self.parse_article_page)
 
         next_page_url = response.css('div.nav_to_paged > a::attr("href")').extract_first()
-        if next_page_url:
+        if next_page_url and  self.crawld_page_count < self.MAX_CRAWL_PAGE_COUNT:
+            self.crawld_page_count += 1
             yield scrapy.Request(next_page_url, callback=self.parse)
 
 
