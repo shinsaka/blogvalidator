ea42c6e259c3a81005b39293b468f22b7f8c320d
shinsaka <shinx1265@gmail.com>
2016-12-12 20:25:12 +0900

    insert result to db(postgres)

--- blogvalidator/pipelines.py
+++ blogvalidator/pipelines.py
@@ -1,7 +1,13 @@
 import os
+import datetime
+from dotenv import load_dotenv, find_dotenv
+import dj_database_url
+import psycopg2
 from . import vision
 from .settings import IMAGES_STORE
 
+
+load_dotenv(find_dotenv())
 
 class SafeValidatorPipeline(object):
     def process_item(self, item, spider):
@@ -9,7 +15,45 @@ class SafeValidatorPipeline(object):
         for image in item['images']:
             image['safe_result'] = vision.safe_search(os.path.join(IMAGES_STORE, image['path']))
             result_images.append(image)
+            self.insert_item(item, image)
 
         item['result_images'] = result_images
 
         return item
+
+    def open_spider(self, spider):
+        p = dj_database_url.config()
+        self.connection = psycopg2.connect(
+            host=p['HOST'],
+            port=p['PORT'],
+            dbname=p['NAME'],
+            user=p['USER'],
+            password=p['PASSWORD'])
+        self.cursor = self.connection.cursor()
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.connection.commit()
+        self.connection.close()
+
+    def insert_item(self, item, image):
+        """
+        insert table from image hash
+        """
+
+        sql = """
+            INSERT INTO crawl_item
+                (name, url, violence, adult, spoof, medical, page_title, page_url)
+            VALUES
+                (%s, %s, %s, %s, %s, %s, %s, %s);
+        """
+        self.cursor.execute(sql, (
+            image['path'],
+            image['url'],
+            image['safe_result']['safeSearchAnnotation']['violence'],
+            image['safe_result']['safeSearchAnnotation']['adult'],
+            image['safe_result']['safeSearchAnnotation']['spoof'],
+            image['safe_result']['safeSearchAnnotation']['medical'],
+            item['title'],
+            item['url'],
+        ))
--- blogvalidator/spiders/image.py
+++ blogvalidator/spiders/image.py
@@ -8,24 +8,36 @@ class ImageSpider(scrapy.Spider):
     start_urls = [
         'http://blog.serverworks.co.jp/tech/category/techical/',
     ]
-    MAX_CRAWL_PAGE_COUNT = 5
-    crawld_page_count = 0
+    LIMIT_PAGES = 5
+    page_count = 0
+    LIMIT_IMAGES = 20
+    image_count = 0
 
     def parse(self, response):
+        # limit pages
+        if self.page_count > self.LIMIT_PAGES:
+            return
+
         for article in response.css('article'):
             article_url = article.css('h1 > a::attr("href")').extract_first()
             yield scrapy.Request(article_url, callback=self.parse_article_page)
 
+
         next_page_url = response.css('div.nav_to_paged > a::attr("href")').extract_first()
-        if next_page_url and self.crawld_page_count < self.MAX_CRAWL_PAGE_COUNT:
-            self.crawld_page_count += 1
+        if next_page_url:
+            self.page_count += 1
             yield scrapy.Request(next_page_url, callback=self.parse)
 
 
     def parse_article_page(self, response):
+        # limit images
+        if self.image_count > self.LIMIT_IMAGES:
+            return
+
         blog_article = BlogArticle()
         blog_article['title'] = response.css('article h1.single-title ::text').extract_first()
         blog_article['url'] = response.url
         blog_article['image_urls'] = [url for url in response.css('article img::attr("src")').extract()]
 
+        self.image_count += len(blog_article['image_urls'])
         yield blog_article
